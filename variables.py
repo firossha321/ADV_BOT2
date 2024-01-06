d2f9e8f89993c51281eb67d4d7ede0f5ffb22ab2
Firos sha muhammad <96859506+firossha321@users.noreply.github.com>
2024-01-06 17:00:29 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -4,19 +4,19 @@ from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6834853102:AAFgAgU7nmAnezFaGKEhIEcEWEg7v67hc6k")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "17432758"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "c9e31fda0ce722e3f3033a9d4f140783")
 
 PICS = os.environ.get("PICS", "https://telegra.ph/file/34fd203eb89fd747ffb57.jpg").split()
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '900873119').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6103642139').split()]
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://firossha321:firossha321@cluster0.xdtpx.mongodb.net/?retryWrites=true&w=majority")
 
-DB_NAME = os.environ.get("DB_NAME", "tg-multi-bot")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 RemoveBG_API = os.environ.get("RemoveBG_API", "")
 
@@ -24,7 +24,7 @@ FORCE_SUB = os.environ.get("FORCE_SUB", None)
 
 HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
  
-log_channel = environ.get("LOG_CHANNEL")
+log_channel = environ.get("LOG_CHANNEL","-1002031293626")
 
 LOG_CHANNEL = int(log_channel) if log_channel and id_pattern.search(log_channel) else None
 
